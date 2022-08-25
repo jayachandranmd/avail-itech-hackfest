498d053644f84709b0e5ed850db0acaee5c6198b
unknown <abhishekarvind07@gmail.com>
2022-08-25 21:58:35 +0530

    Added new colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 Color black = HexColor('#000000');
 Color grey = HexColor('#D9D9D9');
+Color darkgrey = HexColor('#888888');
 Color blue = HexColor('#1100D8');
 Color white = HexColor('#FFFFFF');
 Color red = HexColor('#FF0000');
