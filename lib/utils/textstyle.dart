6e643c0c2e690bb01fd73a1b703c0a3536c02872
unknown <abhishekarvind07@gmail.com>
2022-08-25 22:00:48 +0530

    Added new textstyles

--- lib/utils/textstyle.dart
+++ lib/utils/textstyle.dart
@@ -2,3 +2,5 @@ import 'package:avail_itech_hackfest/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 TextStyle textFieldTitle = TextStyle(color: black, fontSize: 20);
+TextStyle textFieldpara = TextStyle(color: black, fontSize: 16);
+TextStyle headingBoldText = TextStyle(color: black, fontSize: 28,fontWeight: FontWeight.w700);
