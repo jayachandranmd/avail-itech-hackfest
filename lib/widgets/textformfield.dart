6c8e629377fdab22c0f94d5a53c895eb872620a6
unknown <abhishekarvind07@gmail.com>
2022-08-25 22:00:26 +0530

    Added new textformfield parameter added

--- lib/widgets/textformfield.dart
+++ lib/widgets/textformfield.dart
@@ -1,38 +1,55 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
 
 class TextFieldInput extends StatelessWidget {
   final TextEditingController textEditingController;
   final bool isPass;
-  final String hintText;
+  final hintText;
   final validate;
+  final save;
 
   const TextFieldInput({
     Key? key,
     required this.textEditingController,
     this.validate,
     this.isPass = false,
-    required this.hintText,
+    this.hintText,
+    this.save,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final inputBorder = OutlineInputBorder(
-      borderSide: Divider.createBorderSide(context),
-    );
-
-    return TextFormField(
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      validator: validate,
-      controller: textEditingController,
-      decoration: InputDecoration(
-        hintText: hintText,
-        border: inputBorder,
-        focusedBorder: inputBorder,
-        enabledBorder: inputBorder,
-        filled: true,
-        contentPadding: const EdgeInsets.all(8),
+    return SizedBox(
+      height: 100,
+      child: TextFormField(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: validate,
+        onSaved: save,
+        controller: textEditingController,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+            borderSide: BorderSide.none
+          ),
+          focusedBorder:  OutlineInputBorder(
+              borderSide: BorderSide(color:white),
+              borderRadius: BorderRadius.circular(50),
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+              borderSide: BorderSide.none
+          ),
+          filled: true,
+          contentPadding: const EdgeInsets.all(20),
+        ),
+        obscureText: isPass,
       ),
-      obscureText: isPass,
     );
   }
 }
+
