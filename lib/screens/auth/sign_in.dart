d76a69baf4966efff5a659a8732c8a0b8622d85d
jayachandranmd <jayachandranmd.contact@gmail.com>
2022-08-26 09:34:03 +0530

    Added signin method

--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:avail_itech_hackfest/utils/auth_method.dart';
 import 'package:avail_itech_hackfest/utils/colors.dart';
 import 'package:avail_itech_hackfest/utils/constants.dart';
 import 'package:avail_itech_hackfest/utils/textstyle.dart';
@@ -15,148 +16,193 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  TextEditingController emailcontroller =TextEditingController();
-  TextEditingController passwordcontroller =TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+  bool _isObscure = true;
+  @override
+  void dispose() {
+    super.dispose();
+    emailcontroller.dispose();
+    passwordcontroller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Padding(
-            padding: hpad12+hpad12+vpad12+vpad8,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: hpad12 + hpad12 + vpad12 + vpad8,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Login",
-                        style: headingBoldText,
-                      ),
-                      CachedNetworkImage(imageUrl: 'https://firebasestorage.googleapis.com/v0/b/avail-38482.appspot.com/o/logo.png?alt=media&token=f65ddf57-2e6d-4908-bffb-4b0f650aa378',
-                      height: 120,
-                      ),
-                    ],
+                  Text(
+                    "Login",
+                    style: headingBoldText,
                   ),
-                  sBoxH60,
-                  Form(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Email',style: textFieldTitle,),
-                        sBoxH10,
-                        TextFieldInput(textEditingController: emailcontroller, hintText: 'someone@example.com',
-                          validate: (value) {
-                            if (value!.isEmpty) {
-                              return ("Please enter your email");
-                            }
-                            // reg expression for email validation
-                            if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                                .hasMatch(value)) {
-                              return ("Please enter a valid email");
-                            }
-                            return null;
-                          },
-                          save: (value) {
-                            emailcontroller.text = value!;
-                          },
-                        ),
-                        sBoxH5,
-                        Text('Password',style: textFieldTitle,),
-                        sBoxH10,
-                        TextFieldInput(textEditingController: passwordcontroller, hintText: '*****',
-                          validate: (value) {
-                            RegExp regex = RegExp(r'^.{6,}$');
-                            if (value!.isEmpty) {
-                              return ("Password is required for login");
-                            }
-                            if (!regex.hasMatch(value)) {
-                              return ("Enter Valid Password(Min. 6 Character)");
-                            }
-                            return null;
-                          },
-                          save: (value) {
-                            passwordcontroller.text = value!;
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  sBoxH20,
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const SignUp()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      primary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)
-                      )
-                    ),
-                        child: const Text('Login',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w700)),
-                    ),
-                  ),
-                  sBoxH30,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SizedBox(
-                        width: 150,
-                        child: Divider(
-                          thickness: 2,
-                          color: darkgrey,
-                        ),
-                      ),
-                      Text('or',style: TextStyle(color: darkgrey),),
-                      SizedBox(
-                        width: 150,
-                        child: Divider(
-                          thickness: 2,
-                          color: darkgrey,
-                        ),
-                      ),
-                    ],
-                  ),
-                  sBoxH20,
-                  Center(
-                    child:CachedNetworkImage(imageUrl:'https://firebasestorage.googleapis.com/v0/b/avail-38482.appspot.com/o/google.png?alt=media&token=45833eae-dad9-452a-aef1-70bb08cea00d',
-                    height: 40,
-                    )
-                  ),
-                  sBoxH30,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Don't have an account? ",
-                        style:
-                        TextStyle(color: black, fontSize: 18),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUp()));
-                        },
-                        child: InkWell(
-                          child: Text('Sign up',
-                              style: TextStyle(
-                                  color: blue,
-                                  fontSize: 18,
-                                  decoration: TextDecoration.underline)),
-                        ),
-                      )
-                    ],
+                  CachedNetworkImage(
+                    imageUrl:
+                        'https://firebasestorage.googleapis.com/v0/b/avail-38482.appspot.com/o/logo.png?alt=media&token=f65ddf57-2e6d-4908-bffb-4b0f650aa378',
+                    height: 120,
                   ),
                 ],
               ),
-            ),
+              sBoxH60,
+              Form(
+                key: key,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email',
+                      style: textFieldTitle,
+                    ),
+                    sBoxH10,
+                    TextFieldInput(
+                      textEditingController: emailcontroller,
+                      hintText: 'someone@example.com',
+                      validate: (value) {
+                        if (value!.isEmpty) {
+                          return ("Please enter your email");
+                        }
+                        // reg expression for email validation
+                        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                            .hasMatch(value)) {
+                          return ("Please enter a valid email");
+                        }
+                        return null;
+                      },
+                      save: (value) {
+                        emailcontroller.text = value!;
+                      },
+                    ),
+                    Text(
+                      'Password',
+                      style: textFieldTitle,
+                    ),
+                    sBoxH10,
+                    TextFieldInput(
+                      textEditingController: passwordcontroller,
+                      hintText: '*****',
+                      isPass: _isObscure,
+                      validate: (value) {
+                        RegExp regex = RegExp(r'^.{6,}$');
+                        if (value!.isEmpty) {
+                          return ("Password is required for login");
+                        }
+                        if (!regex.hasMatch(value)) {
+                          return ("Enter Valid Password(Min. 6 Character)");
+                        }
+                        return null;
+                      },
+                      save: (value) {
+                        passwordcontroller.text = value!;
+                      },
+                      suffixIcon: IconButton(
+                        color: Colors.white,
+                        icon: Icon(
+                          _isObscure
+                              ? Icons.visibility_outlined
+                              : Icons.visibility_off_outlined,
+                          color: black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              sBoxH20,
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (key.currentState!.validate()) {
+                      AuthMethods().loginUser(
+                          email: emailcontroller.text.trim(),
+                          password: passwordcontroller.text.trim());
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  child: const Text('Login',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                ),
+              ),
+              sBoxH30,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Divider(
+                      thickness: 2,
+                      color: darkgrey,
+                    ),
+                  ),
+                  Text(
+                    'or',
+                    style: TextStyle(color: darkgrey),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: Divider(
+                      thickness: 2,
+                      color: darkgrey,
+                    ),
+                  ),
+                ],
+              ),
+              sBoxH20,
+              Center(
+                  child: CachedNetworkImage(
+                imageUrl:
+                    'https://firebasestorage.googleapis.com/v0/b/avail-38482.appspot.com/o/google.png?alt=media&token=45833eae-dad9-452a-aef1-70bb08cea00d',
+                height: 40,
+              )),
+              sBoxH30,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(color: black, fontSize: 18),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUp()));
+                    },
+                    child: InkWell(
+                      child: Text('Sign up',
+                          style: TextStyle(
+                              color: blue,
+                              fontSize: 18,
+                              decoration: TextDecoration.underline)),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
-        )
-    );
+        ),
+      ),
+    ));
   }
 }
