3f9138898361307610fef806e5005bd42243f850
jayachandranmd <jayachandranmd.contact@gmail.com>
2022-08-27 00:17:58 +0530

    Added new textstyle

--- lib/utils/textstyle.dart
+++ lib/utils/textstyle.dart
@@ -7,4 +7,7 @@ TextStyle contributorText = TextStyle(color: black, fontSize: 15);
 TextStyle subtitle = TextStyle(color: black, fontSize: 13);
 TextStyle username = TextStyle(color: black, fontSize: 17);
 TextStyle textFieldpara = TextStyle(color: black, fontSize: 16);
-TextStyle headingBoldText = TextStyle(color: black, fontSize: 28,fontWeight: FontWeight.w700);
+TextStyle headingBoldText =
+    TextStyle(color: black, fontSize: 28, fontWeight: FontWeight.w700);
+TextStyle selectedText =
+    TextStyle(color: black, fontSize: 20, fontWeight: FontWeight.bold);
